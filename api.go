f846ad469ccc96f2c90e2c3d8fd9f94311fe1f27
Brian Goldman <leonel89011@gmail.com>
2024-08-27 12:33:01 -0300

    Basic API structure working on port 3000

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -62,7 +63,17 @@ func (s *APIServer) Run() {
 
 // handleAccount is a method, which means it is a function associated with a specific type, in this case, APIServer
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	// With the mux router we cannot specify if the request is GET, POST, PUT or DELETE
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("method not allowed: %s", r.Method)
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to gobank!")
+	server := NewAPIServer(":3000")
+	server.Run()
 }
