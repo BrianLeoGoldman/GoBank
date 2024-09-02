4e7df4904f373dceaa97b50397e0b81603896a30
Brian Goldman <leonel89011@gmail.com>
2024-09-02 10:58:18 -0300

    Basic Account creation endpoint working

--- api.go
+++ api.go
@@ -87,7 +87,15 @@ func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) err
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	createAccountReq := new(CreateAccountRequest)
+	if err := json.NewDecoder(r.Body).Decode(createAccountReq); err != nil {
+		return err
+	}
+	account := NewAccount(createAccountReq.Firstname, createAccountReq.Lastname)
+	if err := s.storage.CreateAccount(account); err != nil {
+		return err
+	}
+	return WriteJSON(w, http.StatusCreated, account)
 }
 
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
--- types.go
+++ types.go
@@ -1,13 +1,22 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
-type Account struct {
-	ID        int    `json:"id"`
+type CreateAccountRequest struct {
 	Firstname string `json:"firstname""`
 	Lastname  string `json:"lastname"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+}
+
+type Account struct {
+	ID        int       `json:"id"`
+	Firstname string    `json:"firstname"`
+	Lastname  string    `json:"lastname"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 func NewAccount(firstname string, lastname string) *Account {
@@ -16,5 +25,7 @@ func NewAccount(firstname string, lastname string) *Account {
 		Firstname: firstname,
 		Lastname:  lastname,
 		Number:    int64(rand.Intn(1000000)),
+		// Balance is initialized as zero
+		CreatedAt: time.Now().UTC(),
 	}
 }
